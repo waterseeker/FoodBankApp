8bd5c3a284f50d42b1fe1180707eec955a963cba
Wayne Burris <waterseeker@hotmail.com>
2017-04-11 13:34:14 -0500

    prepping to merge with master

--- js/dataEntry.js
+++ js/dataEntry.js
@@ -1,3 +1,9 @@
-var formData = JSON.stringify($("#customerHousehold").serializeArray());
 
-console.log(formData);
+$("#submitBtn").click(function(){
+    var formData = JSON.stringify($("#customerHousehold").serializeArray());
+    //this should grab the form data and put it into a JSON
+    console.log(formData);
+    console.log("hello");
+});
+
+
