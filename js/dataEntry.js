05aa97ad585cb69201bbd6b60d0f4535360fc649
Wayne Burris <waterseeker@hotmail.com>
2017-04-12 10:24:10 -0500

    got JS to grab values from form on dataEntry.html and write it to json format

--- js/dataEntry.js
+++ js/dataEntry.js
@@ -1,9 +1,28 @@
 
-$("#submitBtn").click(function(){
-    var formData = JSON.stringify($("#customerHousehold").serializeArray());
-    //this should grab the form data and put it into a JSON
-    console.log(formData);
-    console.log("hello");
+$.fn.serializeObject = function()
+{
+    var formObject = {};//empty object
+    var  serializedForm= this.serializeArray(); // serializeArray creates an array of objects (name and value) by serializing form values.
+    $.each(serializedForm, function() { //for each thing in a
+        if (formObject[this.name] !== undefined) { //if it has a name that is not undefined
+            if (!formObject[this.name].push) { //push it to o
+                formObject[this.name] = [formObject[this.name]];
+            }
+            formObject[this.name].push(this.value || ''); //and push either the value or a blank space
+        } else {
+            formObject[this.name] = this.value || '';
+        }
+    });
+    return formObject;
+};
+
+$(function() {
+    $('form').submit(function() { //fires on the submit event on the form
+        $('#result').text(JSON.stringify($('form').serializeObject()));
+      //sets the text of #result
+      //alert("hello");
+        return false;
+    });
 });
 
 
